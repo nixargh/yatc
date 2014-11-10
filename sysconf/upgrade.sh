456b7c5f91a647bad129405b199d5efe0d5bf02d
nixargh <nixargh@gmail.com>
2014-11-11 00:14:40 +0300

    upgrade.sh added

---
+++ sysconf/upgrade.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+# script to upgrade YATC on Ubuntu 12.04 - 14.04
+# (*w) author: nixargh <nixargh@gmail.com>
+VERSION="0.8.2"
+##### Settings ################################################################
+# !!! must be executed from root !!!
+RDPUSER="user"
+FREERDP_REPO="https://github.com/nixargh/FreeRDP.git"
+FREERDP_BRANCH="master"
+YATC_REPO="https://github.com/nixargh/yatc.git"
+YATC_BRANCH="separation"
+TMP_DIR="/tmp"
+###############################################################################
+set -u -e
+
+# Check that you are root.
+#
+check_root() {
+  if [ $USER != "root" ]; then
+    echo "You must be root."
+    return 1
+  fi
+  return 0
+}
+
+# Upgrade FreeRDP.
+#
+upgrade_freerdp() {
+  cd $TMP_DIR
+  git clone $FREERDP_REPO
+  cd ./FreeRDP
+  git checkout $FREERDP_BRANCH
+
+  cmake -DCMAKE_BUILD_TYPE=Debug -DWITH_SSE2=ON -DWITH_CUPS=ON -DCHANNEL_PRINTER=ON -DWITH_PULSE=ON .
+  make
+  make install
+
+  return 0
+}
+
+# Upgrade YATC.
+#
+upgrade_yatc() {
+  local YATCBIN=/usr/bin/yatc
+  local YATCLIB=/usr/lib/yatc
+
+  cd $TMP_DIR
+  git clone $YATC_REPO
+  cd ./yatc
+  git checkout $YATC_BRANCH
+
+  python3 -mpy_compile ./yatc.py
+  mv ./__pycache__/yatc.*.pyc $YATCBIN
+  chmod 755 $YATCBIN
+
+  return 0
+}
+
+# Create user config directory.
+#
+create_config_dir() {
+  local CONF_DIR="/home/$RDPUSER/.yatc"
+
+  if [ ! -d $CONF_DIR ]; then
+    mkdir $CONF_DIR
+    chown $RDPUSER:$RDPUSER $CONF_DIR
+  fi
+  return 0
+}
+
+# Unmute alsa & pulseaudio.
+#
+unmute() {
+  amixer set PCM unmute || echo "Can't unmute PCM. Skipping..."
+  amixer set Master unmute
+  sudo -i -u $RDPUSER pulseaudio -D
+  sleep 1
+  sudo -i -u $RDPUSER pactl set-sink-mute 0 0
+
+  return 0
+}
+##### BEGIN ###################################################################
+
+JOB=$1
+
+echo "Starting upgrade of $1."
+
+check_root || exit 0
+unmute
+create_config_dir
+
+case $JOB in
+  freerdp)
+    upgrade_freerdp
+  ;;
+  yatc)
+    upgrade_yatc
+  ;;
+esac
+
+# fix onership
+chown $RDPUSER:$RDPUSER -R /home/$RDPUSER
+
+echo "Upgraded. Rebooting..."
+
+# Finaly reboot
+reboot
