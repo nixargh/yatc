b0ffa538c58b9aebe072a494e3b7685e67d3adee
nixargh <nixargh@gmail.com>
2015-10-06 23:30:37 +0300

    v0.9.9 branch name fix

--- sysconf/post-install.sh
+++ sysconf/post-install.sh
@@ -9,7 +9,7 @@ TIMEZONE="Europe/Moscow"
 FREERDP_REPO="https://github.com/FreeRDP/FreeRDP.git"
 FREERDP_BRANCH="c9bc88d5f0fed0de03ee697dd382ba8f8a434a82"
 YATC_REPO="https://github.com/nixargh/yatc.git"
-YATC_BRANCH="devel"
+YATC_BRANCH="dev"
 TWOXCLIENT_VER="14.0.3213"
 TWOXCLIENT="http://www.2x.com/downloads/builds/applicationserver/${TWOXCLIENT_VER}/2XClient.deb"
 ###############################################################################
